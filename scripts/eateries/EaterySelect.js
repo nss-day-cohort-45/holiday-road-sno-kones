5722b1d23375271d38df89d9cc8f9891b97a763e
Jordan Carter <jordan.carter.0023@gmail.com>
2020-12-14 15:57:53 -0600

    working on eateries detail button

--- scripts/eateries/EaterySelect.js
+++ scripts/eateries/EaterySelect.js
@@ -12,15 +12,68 @@ export const EateriesSelect = () => {
 const render = eateriesCollection => {
     
     contentTarget.innerHTML = `
-    <select class="dropdown" id="eaterySelect">
-        <option value="0">Please select an eatery...</option>
-        ${
-            eateriesCollection.map((eatery) => `
+    <section class="eateryDetails">
+        <select class="dropdown" id="eaterySelect">
+            <option value="0">Please select an eatery...</option>
+            ${
+                eateriesCollection.map((eatery) => `
           
-                <option value= "${eatery.id}">"${eatery.businessName}"</option>`
-            )
-        }
+                    <option value= "${eatery.id}">"${eatery.businessName}"</option>`
+                )
+            }
         </select>
+        <button type="button value=${eateriesCollection.map((eatery) => ` "${eatery.id}"`)}>Selection Details</button>
+    </section>
     `
 
 }
+
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("click", (event) => {
+    if (event.target.id === "closeDialog") {
+        eateriesDialog.close();
+    }
+})
+
+eventHub.addEventListener("eateriesBtnClicked", (event) => {
+    const eateriesDialog = document.querySelector("#eateriesDialog")
+
+    const dialogText = document.querySelector("#eateriesDialog__text")
+    
+    const eateries = useEateries()
+    
+    const selectedEatery = eateries.find( 
+        (eatery) => eatery.id === parseInt(event.detail.SelectedEateryId))
+
+        dialogText.innerHTML =`
+        <h3>${selectedEatery.businessName}<h3></h3>
+        ${eateriesEatery.map( (eatery) => `
+        <div>${eatery.description}</div>
+        <h4>${eatery.city}, ${eatery.state}</h4>
+        <div>${eatery.amenities}</div>`
+        ).join("")}`
+
+    eateriesDialog.showModal()
+})
+
+export const EateriesDialog = () => {
+    return`
+        <dialog id="eateriesDialog">
+            <div id="associatesDialog__text"></div>
+            <button id="closeDialog">Close</button>
+        </dialog>
+        `
+}
+
+
+eventHub.addEventListener("click", (event) => {
+    if (event.target.id === "eateriesBtnClicked") {
+        const customEvent = new CustomEvent("eateriesBtnClicked", {
+            detail: {
+                SelectedEateryId: event.target.id
+            }
+        })
+        eventHub.dispatchEvent(customEvent)
+    }
+})
