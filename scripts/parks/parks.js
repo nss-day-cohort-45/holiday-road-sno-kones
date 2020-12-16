1c27400c7db03d6d3479f7656bdb8b7e982ef8f2
tcurtis97 <timcurtis97@yahoo.com>
2020-12-16 11:42:05 -0600

    added directions link and description

--- scripts/parks/parks.js
+++ scripts/parks/parks.js
@@ -1,9 +1,9 @@
 export const parkHTMLConverter = (park) => {
   return `
   <h2 class="parkName">${park.fullName}<h2></h2>
-  <div class="parkDirection">${park.directionsUrl}</div>
-  <h4 class"parkLocation">${park.addresses.city}, ${park.addresses.stateCode}</h4>
+  <a class="parkDirections" href="${park.directionsUrl}">Click for directions</a>
   <div class="parkDescription">${park.description}</div>
+
   
  `
 
